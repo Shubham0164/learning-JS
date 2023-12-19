8a50564e5b48658a91555e4f9b310762aeb29558
Shubham Kumar <101205228+Shubham0164@users.noreply.github.com>
2023-12-19 16:41:57 +0000

    done

---
+++ 01_basics/const_let.js
@@ -0,0 +1,7 @@
+const customerId ="13342"
+let custName = "Shubham"
+let custState = "Jharkhand"
+let custDesig = "Student"
+console.table([customerId,custName,custState,custDesig])
+custName = "Shubham Kumar"
+console.table([customerId,custName,custState,custDesig])
